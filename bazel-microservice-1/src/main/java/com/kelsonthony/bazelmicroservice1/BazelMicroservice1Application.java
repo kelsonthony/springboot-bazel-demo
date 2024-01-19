c4e4549d1d193fd3bb0650b95c58772622d42df6
kelsonthony <k.thony@gmail.com>
2024-01-19 17:40:53 +0000

    test demo

--- bazel-microservice-1/src/main/java/com/kelsonthony/bazelmicroservice1/BazelMicroservice1Application.java
+++ bazel-microservice-1/src/main/java/com/kelsonthony/bazelmicroservice1/BazelMicroservice1Application.java
@@ -1,13 +1,21 @@
 package com.kelsonthony.bazelmicroservice1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class BazelMicroservice1Application {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(BazelMicroservice1Application.class, args);
+// 	}
+
+// }
 public class BazelMicroservice1Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BazelMicroservice1Application.class, args);
+		
+		System.out.println("Hello World");
 	}
-
 }
